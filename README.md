91149792766b95c30b2779b8e28b33d7f362ab14
EllaC1234 <ellacaink@gmail.com>
2024-11-27 16:47:20 +0000

    Main code for setting wallpaper

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODO
+
+- Update README.md
+- Handle video content
+- Add to task scheduler
--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 import requests
-import subprocess
+import logging
 import os
 import ctypes
+import subprocess
 
-def set_wallpaper(image_path):
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{image_path}" , 0)
+def set_wallpaper(image_path, style):
+    powershell_cmd = f"""
+        Set-ItemProperty -Path "HKCU:\\Control Panel\\Desktop" -Name WallpaperStyle -Value 6 -Force
+        RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters 1, True
+    """
+
+    process = subprocess.Popen(["powershell", "-Command", powershell_cmd])
+    process.wait()
+
+    full_path = os.path.join(os.getcwd(), image_path)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, full_path, 0)
 
 
-def get_apod_image():
-    api_url = "https://api.nasa.gov/planetary/apod"
+def download_image(image_url, filename):
+    response = requests.get(image_url)
+
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+
+def fetch_image_data(endpoint, endpoint_params):
+    try:
+        response = requests.get(endpoint, endpoint_params)
+        response.raise_for_status()
+
+        return response.json()[0]['hdurl']
+    
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        return None
+
+
+def main(endpoint, params, filename, style):
+    logging.basicConfig(filename='daily_image_error.log', level=logging.ERROR)
+
+    image_url = fetch_image_data(endpoint, params)
+    if image_url:
+        download_image(image_url, filename)
+        set_wallpaper(filename, style)
+
+if __name__ == "__main__":
+    api_key = os.environ.get('NASA_APOD_API_KEY')
+    if not api_key:
+        api_key = "DEMO_KEY"
+
+    endpoint = "https://api.nasa.gov/planetary/apod"
     params = {
-        "api_key": "DEMO_KEY"
+        "api_key": api_key,
+        "start_date": "2024-11-26",
+        "end_date": "2024-11-26"
     }
+    filename = "nasa_apod_image.jpg"
+    style = 6
 
-    response = requests.get(api_url, params)
-
-    if response.status_code == 200:
-        image_url = response.json()['url']
-        response = requests.get(image_url)
-
-        with open("./nasa_apod_image.jpg", "wb") as f:
-            f.write(response.content)
-        
-        set_wallpaper("nasa_apod_image.jpg")
-        # os.remove("nasa_apod_image.jpg")
-
-get_apod_image()
+    main(endpoint, params, filename, style)
